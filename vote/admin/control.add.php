6853ca38237fde881a126c0182e8bc014d181b37
Derek Spranger <derek.spranger@pirateparty.org.au>
2010-09-26 23:37:11 +1000

    added save functionality

--- vote/admin/control.add.php
+++ vote/admin/control.add.php
@@ -1,5 +1,58 @@
+<?php
+	if( !empty( $_POST ) )
+	{
+		if( strcmp( $_POST['password'], $_POST['confirm'] ) !== 0 )
+		{
+			$error = 'Passwords do not match up';
+		}
+		else
+		{
+			$conf = get_config();
+			$db = get_db();
+			
+			$sql = sprintf( '
+				SELECT
+					id
+				FROM
+					form_vote_admins
+				WHERE
+					username = \'%s\'
+				LIMIT
+					1
+				',
+				$_POST['username']
+			);
+			
+			$user = $db->GetRow( $sql );
+			
+			if( !empty( $user ) )
+			{
+				$error = 'Username already exists';
+			}
+			else
+			{
+				$sql = sprintf( '
+					INSERT INTO
+						form_vote_admins
+					SET
+						username = \'%s\',
+						password = SHA1( \'%s\' )
+					',
+					$_POST['username'],
+					$conf['salt_start'] . $_POST['password'] . $conf['salt_end']
+				);
+				
+				$db->Execute( $sql );
+				
+				header( 'Location: /admin/control/' );
+				die();
+			}
+		}
+	}
+?>
 Add new admin account.
 <div class="gap"></div>
+<?php echo $error; ?>
 <form action="/admin/control/add/" enctype="application/x-www-form-urlencoded" method="post">
 	<table>
 		<tr>
@@ -23,4 +76,3 @@ Add new admin account.
 		</tr>
 	</table>
 </form>
-<?php
