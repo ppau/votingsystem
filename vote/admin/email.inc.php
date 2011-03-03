4169499aeacfcf6ba769925e7b7e7f364ef8888a
Sam Dunster <samdunster@pirateparty.org.au>
2011-03-03 16:15:46 +1100

    Made email script run within admin CP - removed auth since this should be handled by admin CP stuff (NOTE: currently flawed security - must review)

--- vote/admin/email.inc.php
+++ vote/admin/email.inc.php
@@ -1,35 +1,9 @@
 <?php
-	include_once dirname( __FILE__ ) . '/Smarty/libs/Smarty.class.php';
-	include_once dirname( __FILE__ ) . '/Swift-4.0.6/lib/swift_required.php';
-	include_once dirname( __FILE__ ) . '/database.php';
-	require_once dirname( __FILE__ ) . '/src/group.php';
-	require_once dirname( __FILE__ ) . '/src/standardCurves.php';
-	
-	function passwordProtect( $username, $password )
-	{
-		if (
-				(
-					!isset( $_SERVER['PHP_AUTH_USER'] ) ||
-					(
-						isset( $_SERVER['PHP_AUTH_USER'] ) && $_SERVER['PHP_AUTH_USER'] != $username
-					)
-				) &&
-				(
-					!isset( $_SERVER['PHP_AUTH_PW'] ) ||
-					(
-						isset( $_SERVER['PHP_AUTH_PW'] ) && $_SERVER['PHP_AUTH_PW'] != $password
-					)
-				)
-			)
-		{
-			header( 'WWW-Authenticate: Basic realm="Login"' );
-			header( 'HTTP/1.0 401 Unauthorized' );
-			echo 'Please login to continue.';
-			exit;
-		}
-	}
-	
-	passwordProtect( 'admin', 'bNyXRcP1DT7d0JVlxBgJ3BTz' );
+	include_once dirname( __FILE__ ) . '/../Smarty/libs/Smarty.class.php';
+	include_once dirname( __FILE__ ) . '/../Swift-4.0.6/lib/swift_required.php';
+	include_once dirname( __FILE__ ) . '/../database.php';
+	//require_once dirname( __FILE__ ) . '/src/group.php';
+	//require_once dirname( __FILE__ ) . '/src/standardCurves.php';
 	
 	//Create the Transport
 	//$transport = Swift_MailTransport::newInstance();
