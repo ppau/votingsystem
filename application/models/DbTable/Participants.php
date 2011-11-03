37f745a198c75c9163c8b4336c57184d4e494782
Sam Dunster <me@sdunster.com>
2011-11-04 01:09:37 +1100

    Added some methods to participants model

--- application/models/DbTable/Participants.php
+++ application/models/DbTable/Participants.php
@@ -3,8 +3,17 @@
 class App_Model_DbTable_Participants extends Zend_Db_Table_Abstract
 {
 
-    protected $_name = 'participants';
+	protected $_name = 'participants';
 
+	public function fetchNeedKeys()
+	{
+		return $this->fetchAll($this->select()->where('mailed = 0'));	
+	}
+
+	public function fetchNoKeys()
+	{
+		return $this->fetchAll($this->select()->where('mailed != 0'));	
+	}
 
 }
 
