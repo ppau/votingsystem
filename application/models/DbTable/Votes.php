35c7edcc04f2c2b765eee9803b124f1a308eb252
Sam Dunster <me@sdunster.com>
2011-11-14 21:14:46 +1100

    Added dump script to dump vote data

--- application/models/DbTable/Votes.php
+++ application/models/DbTable/Votes.php
@@ -6,5 +6,9 @@ class App_Model_DbTable_Votes extends Zend_Db_Table_Abstract
     protected $_name = 'votes';
 
 
+	public function fetchVotesForPoll($pollid)
+	{
+		return $this->fetchAll($this->select()->where('pollid = ?',(int)$pollid));
+	}
 }
 
---
+++ scripts/dump.php
@@ -0,0 +1,46 @@
+#!/usr/bin/php
+<?php
+
+require_once './init.php';
+#require_once 'Crypto/group.php';
+#require_once 'Crypto/standardCurves.php';
+
+function usage()
+{
+	echo 'Usage: ./dump.php <pollid>'."\n";
+}
+
+if($argc != 2 || !is_numeric($argv[1]))
+{
+	usage();
+	die();
+}
+
+$db = new App_Model_DbTable_Polls();
+$polls = $db->find($argv[1]);
+
+if(count($polls) == 0)
+{
+	die("Specified poll doesn't exist\n");
+}
+
+$poll = $polls->getRow(0);
+
+$db = new App_Model_DbTable_Votes();
+$votes = $db->fetchVotesForPoll($poll->id);
+
+$dump = array();
+
+foreach($votes as $v)
+{
+	if(json_decode($v->data) === NULL)
+	{
+		fwrite(STDOUT, $v->id . ": invalid JSON - skipping\n");
+		continue;
+	}
+
+	$dump[] = $v->data;
+}
+
+echo "[\n" . implode(",", $dump) . "]\n";
+
